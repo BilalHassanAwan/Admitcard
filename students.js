24068686c861790542d70de2d3852fc92a5bd805
BilalAwan <Bilalhassanawan40@gmail.com>
2025-11-30 21:29:32 +0500

    Add files via upload

---
+++ students.js
@@ -0,0 +1,15 @@
+const students = {
+    "B24F0098CS010": {
+        name: "Bilal Awan",
+        program: "BS Computer Science",
+        semester: 3,
+        photo: "bilal.jpg",
+        exams: [
+            { date: "20/10/2025", paper: "DSA (COMP-201)", venue: "A1-307", time: "10:00 AM" },
+            { date: "20/10/2025", paper: "Professional Ethics (SS-311)", venue: "A1-207", time: "03:00 PM" },
+            { date: "21/10/2025", paper: "P&S (MTH-210)", venue: "A2-301", time: "10:00 AM" },
+            { date: "22/10/2025", paper: "Database Systems (COMP-231)", venue: "B2-211", time: "10:00 AM" },
+            { date: "23/10/2025", paper: "Digital Logic (ECE-161)", venue: "C2 Ground Floor", time: "10:00 AM" }
+        ]
+    }
+};
